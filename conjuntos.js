4f78c42481e3f5821446af6c0a8438ecc31a4ded
Gastón Leandro Pereyra <leandro-pereyra@hotmail.es>
2022-06-13 16:45:36 +0200

    Ejercicio 7 - 1 - JS

---
+++ conjuntos.js
@@ -0,0 +1,8 @@
+const familia = new Set(["Leandro", "Romina", "Angela", "Reyes"])
+console.log(familia)
+
+familia.add("Leandro")
+console.log(familia)
+
+familia.add("Javascript")
+console.log(familia)
